f2fb217854622cc7a96799a61252e495357ee5e7
Jude Kuti <jkuti@sei-mi.com>
2019-04-04 17:54:56 -0500

    Fix tagged literal function

--- weather/app.js
+++ weather/app.js
@@ -48,13 +48,43 @@ document.querySelector("#pulse").addEventListener('click', function () {
                         var weather_id = document.querySelector("#weather-body");
                         var weather_id2 = document.querySelector("#weather-body2");
                         var num = 0;
+
+                        // create usable function with template literals
+                        // note that the first thing in tagged literals is a string literal and the second the parameter
+                        function weather_icon_check(literal, short_forecast){                            
+                            var weather_icon = " "; 
+                            if(short_forecast.indexOf('Rain') != -1){
+                                weather_icon = './img/rain-clouds.png';
+                            }else if(short_forecast.indexOf('Clear') !== -1) {
+                                weather_icon = './img/moon-and-clouds.png';
+                            } else if (short_forecast.indexOf('Sunny') !== -1) {
+                                weather_icon = './img/snow-clouds.png';
+                            } else if (short_forecast.indexOf('Thunderstorms') !== -1) {
+                                weather_icon = './img/stormrain-thunders.png';
+                            } else {
+                                weather_icon = './img/eclipse.png';
+                            }
+                            console.log("weather icon displayed: ", weather_icon);
+                            console.log("short forecast displayed: ", short_forecast);
+                            return `${weather_icon}`;
+                        }
+
+                        console.log("Checking short forecast: ", full_data[num+1].shortForecast);
+
+                        // var initial_data = full_data.slice(0,3);
+                        // // console.log(fdata);
+
+                        // // return alternate indices
+                        // let first_data = intial_data.filter(function(element, index){
+                        //     return index % 2 == 0;
+                        // });
                         weather_id.innerHTML = `
                                 <div class="col-md-6 py-3">
                                     <div class="row">
                                         <div class="col-12 pb-2">
                                             <div class="card transp text-center">
                     <h3 class="card-header current-city-weather">${full_data[num].temperature}<sup class="font-weight-light">o <sub>F</sub></sup> | ${full_data[num].shortForecast}</h3>
-                                                <img class="current-city-weather" style="margin: 0 auto;" width="150px;" src="" alt="Card image">
+                                                <img class="current-city-weather" style="margin: 0 auto;" width="150px;" src="${weather_icon_check`${full_data[num].shortForecast}`}" alt="Card image">
                                                 <h4 class="current-city-weather px-0">${full_data[num].name}</h4>
                                                 <h6 class="current-city-weather px-3 text-justify">${full_data[num].detailedForecast}</h6>
                                                 <div class="card-footer">
@@ -70,7 +100,7 @@ document.querySelector("#pulse").addEventListener('click', function () {
                                                 <div class="col-4 sm-days">
                                                     <div class="card transp text-center">
                                                         <h6 class="card-header hc px-0">${full_data[num+1].name}</h6>
-                                                        <img class="current-city-weather" style="margin: 0 auto;" width="80px;" src="${weather_icon}"
+                                                        <img class="current-city-weather" style="margin: 0 auto;" width="80px;" src="${weather_icon_check`${full_data[num+1].shortForecast}`}"
                                                             alt="Card image">
                                                         <div class="card-footer">
                                                         ${full_data[num+1].temperature}<sup>o<sub>F</sub></sup>
@@ -80,7 +110,7 @@ document.querySelector("#pulse").addEventListener('click', function () {
                                                 <div class="col-4 sm-days">
                                                     <div class="card transp text-center">
                                                         <h6 class="card-header hc px-0">${full_data[num+2].name}</h6>
-                                                        <img class="current-city-weather" style="margin: 0 auto;" width="80px;" src="${weather_icon}"
+                                                        <img class="current-city-weather" style="margin: 0 auto;" width="80px;" src="${weather_icon_check`${full_data[num+2].shortForecast}`}"
                                                             alt="Card image">
                                                         <div class="card-footer">
                                                         ${full_data[num+2].temperature}<sup>o<sub>F</sub></sup>
@@ -90,7 +120,7 @@ document.querySelector("#pulse").addEventListener('click', function () {
                                                 <div class="col-4 sm-days">
                                                     <div class="card transp text-center">
                                                         <h6 class="card-header hc">${full_data[num+3].name}</h6>
-                                                        <img class="current-city-weather" style="margin: 0 auto;" width="80px;" src="${weather_icon}"
+                                                        <img class="current-city-weather" style="margin: 0 auto;" width="80px;" src="${weather_icon_check`${full_data[num+3].shortForecast}`}"
                                                             alt="Card image">
                                                         <div class="card-footer">
                                                         ${full_data[num+3].temperature}<sup>o <sub>F</sub></sup>
@@ -105,9 +135,7 @@ document.querySelector("#pulse").addEventListener('click', function () {
                                     <span class="loc">${city}, ${state}</span>
                                         <img style="margin: 0 auto;" width="120px;" src="./img/sun.png" alt="Card image"> 
                                         <h4></h4> 
-                                </div>`
-
-
+                                </div>`;
 
                         // create handler to return day values only using name values
                         var nums = 0;
@@ -127,7 +155,7 @@ document.querySelector("#pulse").addEventListener('click', function () {
                         
                         // delete first two indices of array
                         var fdata = full_data.slice(2);
-                        console.log(fdata);
+                        // console.log(fdata);
 
                         // return alternate indices
                         let alter_data = fdata.filter(function(element, index){
@@ -135,7 +163,7 @@ document.querySelector("#pulse").addEventListener('click', function () {
                         });
                        
                         var weather_icon = " ";
-                        // use template literals to create and array of 6 items and then itereate through using map
+                        // use template literals to create and array of number of items and then itereate through using map
                         
                         // Alternative I: Array(5).fill() instead of Array(5).join(0).split(0)
                         // Alternative II: Array(5 + 1).fill('').reduce((finalMarkup) => finalMarkup + `stuff to be repeated \n`);
@@ -164,7 +192,7 @@ document.querySelector("#pulse").addEventListener('click', function () {
                         // `.trim()).join('')}
                         // `
                         weather_id2.innerHTML = `  
-                        ${Array(6).join(0).split(0).map(function(item , i){
+                        ${Array(alter_data.length).join(0).split(0).map(function(item , i){
 
                             if (alter_data[i].shortForecast.indexOf('Rain') !== -1) {
                                 weather_icon = './img/rain-clouds.png';
